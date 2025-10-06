f4a2955d61c86ec7534b42526b754c25897f893a
Liem-R04 <baliem0404@gmail.com>
2025-10-06 12:17:11 +1100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
-#include <Arduino.h>
+#include "Lora.h"
 
+#define LORA_SCK   18 // connect 5v and gnd for lora and gps too
+#define LORA_MISO  19
+#define LORA_MOSI  23
+#define LORA_CS    5
+#define LORA_RST   26
+#define LORA_IRQ   27 //G0 pin
+
+#define GPS_RX     16
+#define GPS_TX     17
+
+LoraGPS loraGPS;
 
 void setup() {
-  // put your setup code here, to run once:
+    loraGPS.begin();  // just call begin, pins are defined here
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    loraGPS.update();
 }
